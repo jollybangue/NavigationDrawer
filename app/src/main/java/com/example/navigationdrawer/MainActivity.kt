6c20a27bc56178181f7c39f8ab4de14f0c83a643
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-09-25 17:49:51 -0600

    Initial commit
    A small Android app to experiment Navigation Drawer features.

---
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.navigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        var nvDrawer = findViewById<NavigationView>(R.id.nvDrawer)
+
+        nvDrawer.setNavigationItemSelectedListener {
+            Toast.makeText(applicationContext, "You chose: ${it.title}", Toast.LENGTH_SHORT).show()
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
